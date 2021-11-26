0c72d3ad9946ec7e9d2a01ea984ce52f3c467ded
Jaguarrr-111 <92041341+Jaguarrr-111@users.noreply.github.com>
2021-11-26 18:22:02 +0530

    Update plot1and2.cpp

--- plot1and2.cpp
+++ plot1and2.cpp
@@ -1,12 +1,8 @@
 void plot1and2()
 {
     TFile *dataset = TFile::Open("13TeV_CR0_RHoff.root");
- 
-    TH1 *plot1 = new TH1F("plot1", "Plot - 2", 192, 0, 192);
-    TH1 *plot2 = new TH1F("plot2", "Plot - 1", 192, 0, 192);
 
-    const char* tree_names[] = {
-        "pytree",
+    const char* tree_plot_names[] = {
         "pytree020",
         "pytree2040",
         "pytree4060",
@@ -15,47 +11,84 @@ void plot1and2()
         "pytree100"
     };
 
-    const int tree_sizes[] = {192, 20, 40, 60,  80, 100, 192};
+    const int tree_sizes[] = {20, 40, 60,  80, 100, 192};
 
-    for (int i = 1; i < 7; i++)
+    auto c1 = new TCanvas("c1", "Plot-1", 800, 600);
+    auto c2 = new TCanvas("c2", "Plot-2", 800, 600);
+    c1->Divide(3, 2);
+    c2->Divide(3, 2);
+    
+    TH1* plot1[6];
+    TH1* plot2[6];
+    
+
+    for (int i = 0; i < 6; i++)
     {
-        std::cout << "processing tree: " << i+1 << " of 7 " << '\n';
-        read_from_tree_variable<double> v_eta("eta", tree_names[i], dataset, tree_sizes[i]);
-        read_from_tree_ntrack v_ntrack(tree_names[i], dataset);
+        char plot_name[256];
         
-        int size = v_ntrack.max_index();
-        for (int j = 0; j < size; j++)
+        if (i != 5)
+        {
+            sprintf(plot_name, "%d to %d", i*20, (i+1)*20);
+            plot1[i] = new TH1F(plot_name, "", 20, i*20, (i+1)*20);
+
+            sprintf(plot_name, " %d to %d ", i*20, (i+1)*20);
+            plot2[i] = new TH1F(plot_name, "", 20, i*20, (i+1)*20);
+        }
+        else
+        {
+            sprintf(plot_name, "%d to %d", 192-101, 192);
+            plot1[i] = new TH1F(plot_name, "", 192-101, 101, 192);
+
+            sprintf(plot_name, " %d to %d ", 192-101, 192);
+            plot2[i] = new TH1F(plot_name, "", 192-101, 101, 192);
+        }
+         
+
+        read_from_tree_variable<double> v_eta("eta", tree_plot_names[i], dataset, tree_sizes[i]);
+        read_from_tree_variable<double> v_pT("pT", tree_plot_names[i], dataset, tree_sizes[i]);
+        read_from_tree_ntrack v_ntrack(tree_plot_names[i], dataset);
+        
+        int max_index = v_ntrack.max_index();
+        
+        for (int j = 0; j < max_index; j++)
         {
             double* eta = v_eta.get(j);
+            double* pT = v_pT.get(j);
             int ntrack = v_ntrack.get(j);
 
             for (int k = 0; k < ntrack; k++)
             {
-                if (eta[k] < -1 || eta[k] > 1)
+                if (0.5 < pT[k] || pT[k] < -0.5)
                 {
-                    plot1->Fill(ntrack);
-                   // if (ntrack > 120)
-                        //std::cout << ntrack << '\n';
-                }
-                else
-                {
-                    plot2->Fill(ntrack);
+                    if (eta[k] < -1 || eta[k] > 1)
+                    {
+                        plot1[i]->Fill(ntrack);
+                    }
+                    else
+                    {
+                        plot2[i]->Fill(ntrack);
+                    }
                 }
             }
+
+            plot1[i]->SetFillColor(kYellow);
+            c1->cd(i+1);
+            plot1[i]->Draw();
+
+            plot2[i]->SetFillColor(kYellow);
+            c2->cd(i+1);
+            plot2[i]->Draw();
+            
         }
     }
-    auto c1 = new TCanvas("c1","Canvas",800,600);
-    c1->Divide(2,1);
 
-    plot1->SetFillColor(kYellow);
-    plot1->GetYaxis()->SetTitle("Number of events in acceptance region");
-    plot1->GetXaxis()->SetTitle("Multiplicity");
-    c1->cd(1);
-    plot1->Draw();
+    plot1[0]->GetYaxis()->SetTitle("Frequency");
+    plot1[0]->GetYaxis()->SetTitleSize(0.06);
+    plot1[5]->GetXaxis()->SetTitle("Multiplicity (|\\eta| > 1)  ");
+    plot1[5]->GetXaxis()->SetTitleSize(0.06);
 
-    plot2->SetFillColor(kYellow);
-    plot2->GetYaxis()->SetTitle("Number of events not in acceptance region");
-    plot2->GetXaxis()->SetTitle("Multiplicity");
-    c1->cd(2);
-    plot2->Draw();
+    plot2[0]->GetYaxis()->SetTitle("Frequency");
+    plot2[0]->GetYaxis()->SetTitleSize(0.06);
+    plot2[5]->GetXaxis()->SetTitle("Multiplicity (|\\eta| < 1)  ");
+    plot2[5]->GetXaxis()->SetTitleSize(0.06);
 }
